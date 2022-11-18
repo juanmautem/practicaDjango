8ee375aee614e28b9e744eac2fe5b01e92985336
Juan Manuel Fernández Alvarez <manuel-fernandez@utem.edu.mx>
2022-11-18 15:09:16 -0600

    Nombre atributos

--- applications/noticias/admin.py
+++ applications/noticias/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(noticia)
-admin.site.register(picture)
+
+class noticiaAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'txtNotice',
+		'eIdUser',
+		'bActive',
+		'fhCreatedAt',
+	)
+
+class pictureAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'txtName',
+		'txtUrl',
+		'eIDNotice',
+		'fhCreatedAt',
+	)
+
+admin.site.register(noticia,noticiaAdmin)
+admin.site.register(picture, pictureAdmin)
--- applications/usuarios/admin.py
+++ applications/usuarios/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(usuario)
+class usuarioAdmin(admin.ModelAdmin):
+	list_display = (
+		'eIdPersona',
+		'txtUser',
+		'txtPass',
+		'txtUserType',
+		'fhCreatedAt',
+		'bActive',
+	)
+
+admin.site.register(usuario,usuarioAdmin)
